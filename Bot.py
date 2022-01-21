d3aa008d40c85e948698baaba93e3b752636cb20
Thiago-Lir-a <thiago0257t@gmial.com>
2022-01-20 19:42:46 -0500

    vai

--- Bot.py
+++ Bot.py
@@ -1,3 +1,24 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome import options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
 
-driver = driver.chrome()
+
+url = 'http://diario.ac.gov.br/'
+
+# abrindo navegador e passando o endereço
+driver = webdriver.Chrome()
+driver.get(url)
+
+# buscando elemento de download por click
+download_capa = driver.find_element(By.CLASS_NAME, 'pdficon')
+download_capa.click()
+
+# buscando elemento de
+numero_diario = driver.find_element(By.CLASS_NAME, 'txt-hj-pequeno')
+print(numero_diario)
+
+sleep(150)
+driver.close()
