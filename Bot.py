791ddd8f131cb965b333c391c64080e31d98a877
Thiago-Lir-a <thiago0257t@gmial.com>
2022-01-20 21:49:43 -0500

    50%

--- Bot.py
+++ Bot.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.driver import Driver
 from selenium import webdriver
 from selenium.webdriver.chrome import options
 from selenium.webdriver.common.keys import Keys
@@ -16,9 +17,22 @@ driver.get(url)
 download_capa = driver.find_element(By.CLASS_NAME, 'pdficon')
 download_capa.click()
 
-# buscando elemento de
+# buscando o número do diário para uso posterior
 numero_diario = driver.find_element(By.CLASS_NAME, 'txt-hj-pequeno')
+numero_diario = numero_diario.text.split()
+numero_diario = numero_diario[3]
 print(numero_diario)
 
-sleep(110)
+# encontrando a caixa de busca por número, passando o numero e a tecla enter
+caixa_busca = driver.find_element(By.ID, "numero")
+caixa_busca.click()
+caixa_busca.send_keys(numero_diario)
+caixa_busca.send_keys(Keys.ENTER)
+sleep(10)
+
+# buscando o arquivo para download
+
+download = driver.find_element(By.PARTIAL_LINK_TEXT, "http://diario.ac.gov")
+download.click()
+
 driver.close()
