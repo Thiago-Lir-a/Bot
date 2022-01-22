63a3d17be369f460f400db91f1f15c29a27920c4
Thiago-Lir-a <thiago0257t@gmial.com>
2022-01-21 21:03:34 -0500

    adição da funcionalidade de encontrar o local do download

--- Bot.py
+++ Bot.py
@@ -15,24 +15,35 @@ driver.get(url)
 
 # buscando elemento de download por click
 download_capa = driver.find_element(By.CLASS_NAME, 'pdficon')
-download_capa.click()
+# download_capa.click()
 
 # buscando o número do diário para uso posterior
 numero_diario = driver.find_element(By.CLASS_NAME, 'txt-hj-pequeno')
 numero_diario = numero_diario.text.split()
 numero_diario = numero_diario[3]
-print(numero_diario)
 
 # encontrando a caixa de busca por número, passando o numero e a tecla enter
 caixa_busca = driver.find_element(By.ID, "numero")
 caixa_busca.click()
 caixa_busca.send_keys(numero_diario)
 caixa_busca.send_keys(Keys.ENTER)
-sleep(10)
+sleep(1)
 
-# buscando o arquivo para download
+# iniciando o loop para baixar os pdf's
 
-download = driver.find_element(By.PARTIAL_LINK_TEXT, "http://diario.ac.gov")
-download.click()
+while numero_diario > 13072:
 
-driver.close()
+    # buscando o arquivo para download
+download = driver.find_elements(By.CSS_SELECTOR, '[title="Fazer download"]')
+cont = len(download)
+print(cont)
+
+# download[0].click()
+# sleep(10)
+# download[1].click()
+# sleep(10)
+# download[2].click()
+# sleep(10)
+# download[3].click()
+# sleep(10)
+# driver.close()
